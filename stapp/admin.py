c34c3b7df5a901e183386ac22c49aac44c8b066c
Ryosuke Surface <r-shimada0930@gmail.com>
2021-08-16 22:28:28 +0900

    都道府県テーブルの作成、CSV入出力機能追加

--- stapp/admin.py
+++ stapp/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Prefecture
+
+# django-import-exportsの設定
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from import_export.formats import base_formats
+
 
 # Register your models here.
+
+
+class PrefectureResource(resources.ModelResource):
+    class Meta:
+        model = Prefecture
+
+
+class PrefectureAdmin(ImportExportModelAdmin):
+    # django-import-exportsの設定
+    resource_class = PrefectureResource
+    formats = [base_formats.CSV] # formatsで指定できる
+    
+    # adminでの表示項目
+    list_display = ('id','prf_no','prf_name')
+    list_filter = ['prf_no']
+    search_fields = ['prf_name']
+
+admin.site.register(Prefecture, PrefectureAdmin)
--- stapp/models.py
+++ stapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+'''class Station(models.Model):
+    station_name_hira = models.CharField(max_length=100)
+    station_name_kanji = models.CharField(max_length=100)
+    station_name_kata = models.CharField(max_length=100)
+    first_letter = models.CharField(max_length=1)
+    last_letter = models.CharField(max_length=1)
+    isends_n = models.BooleanField
+    prf_no = models.IntegerField
+    city_name = models.CharField(max_length=100)
+    jr_co_name = models.CharField(max_length=100)
+    rail_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.station_name_kanji
+        '''
+
+class Prefecture(models.Model):
+    prf_no = models.IntegerField(default=0)
+    prf_name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.prf_name
