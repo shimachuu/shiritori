04fbb9cd8c9255e38efd1a18720ac53a1fd57362
Ryosuke Surface <r-shimada0930@gmail.com>
2021-08-29 10:22:12 +0900

    フィルターと保存,変数を渡す＝しりとり骨組み完成

--- stapp/forms.py
+++ stapp/forms.py
@@ -16,18 +16,24 @@ class TestChoiceForm(forms.ModelForm):
         model = TestChoice
         fields = ('game','station')
 
-    def __init__(self, game_id, start_letter, *args, **kwargs):
+    # formが画面に表示される前に何をするかを定める
+    def __init__(self, *args, **kwargs):
+        # https://hideharaaws.hatenablog.com/entry/2017/02/05/021111
+        self.start_letter = kwargs.pop('start_letter')
+        self.game_id  = kwargs.pop('game')
         
         # https://sleepless-se.net/2018/07/10/django%E3%83%95%E3%82%A9%E3%83%BC%E3%83%A0%E5%86%85%E3%81%AE%E9%96%A2%E9%80%A3%E3%83%86%E3%83%BC%E3%83%96%E3%81%AB%E3%83%95%E3%82%A3%E3%83%AB%E3%82%BF%E3%83%BC%E3%82%92%E3%81%8B%E3%81%91%E3%82%8B/
-        # initメソッドを丸っと継承する（意味不明だがないと動かない）
+        # ModelForm（親クラス）のinitメソッドを丸っと継承する（意味不明だがないと動かない）
         super().__init__(*args, **kwargs)
         
         # start_letter = kwargs.get("instance").start_letter
         # start_letter = kwargs.pop('start_letter')
-        self.start_letter = start_letter # 成功！
-        self.fields['game'].initial = game_id
+        
+        self.fields['game'].initial = self.game_id
 
         # https://codor.co.jp/django/about-filter
-        self.fields['station'].queryset = MstTestStation.objects.filter(first_letter=start_letter)
+        self.fields['station'].queryset = MstTestStation.objects.filter(first_letter=self.start_letter)
+
+        # super().__init__(*args, **kwargs)
 
     
--- stapp/views.py
+++ stapp/views.py
@@ -7,14 +7,15 @@ from .forms import TestGameForm, TestChoiceForm
 
 from django.http import HttpResponse
 
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView
 
 # Create your views here.
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
-
+# new_gameもFormViewで書き換えられると思う
+# render(url)での変数受け渡しを脱する必要がある
 def new_game(request):
 
     if request.method == "POST":
@@ -55,14 +56,14 @@ def new_game(request):
 
 
 
-def new_choice(request):
+''' def new_choice(request):
     
-    '''
+    
     # new_gameもしくは前の選択から受け取った変数paramsを分解
     # https://djangobrothers.com/blogs/django_redirect_with_parameters/
     game_id = request.GET.get('game') # gameID。これはしりとりが続く限り引き継いでいく秘伝のタレ
     start_letter = request.GET.get('start_letter') # 最初の文字。querysetをfilterする
-    '''
+    
 
 
     if request.method == "POST":
@@ -75,7 +76,7 @@ def new_choice(request):
         start_letter = MstTestStation.objects.get(pk=station_id).last_letter
     
         
-        form = TestChoiceForm(game_id, start_letter, data=request.POST) # ← 受け取ったPOSTデータを渡す
+        form = TestChoiceForm(game_id=game_id, start_letter=start_letter, data=request.POST) # ← 受け取ったPOSTデータを渡す
         
         #入力内容のチェック
         if form.is_valid():
@@ -100,11 +101,11 @@ def new_choice(request):
 
             return render(request, 'new_choice.html', {'form': form})
             
-            # '''
+            # 
             #return redirect(url)
         else:
             return HttpResponse("invalid") # なぜだ…
-    else: # ← methodが'POST'ではない = 最初のページ表示時の処理
+    else: # GET メソッドで受け取った場合の処理 = 最初のページ表示時の処理
         
         # new_gameもしくは前の選択から受け取った変数paramsを分解
         # https://djangobrothers.com/blogs/django_redirect_with_parameters/
@@ -120,27 +121,61 @@ def new_choice(request):
 
     # return render(request, 'new_choice.html', params)
 
+'''
+
+'''
+流れ（HTTP GET）
+    as_view()で呼ばれて、HTTP methodがGETの場合、ProcessFormViewのdef getが呼ばれる
+    FormMixin中のget_context_data()が呼ばれ、インスタンス変数のform_classがkwargs['form']に格納される
+    今までと同様、templateをreturnする
+流れ（HTTP POST）
+    as_view()で呼ばれて、HTTP methodがPOSTの場合、ProcessFormViewのdef postが呼ばれる
+    FormMixinのget_form()が呼ばれ、値の挿入されたformを入手
+    form.is_valid()でform classで定義されたvalidationの判定
+    validであればFormMixinのform_valid(form)が呼ばれ, インスタンス変数のsuccess_urlにリダイレクトされる
+    invlidであればエラーと一緒にGETにリダイレクトされる         
+'''
 
 
+# レコードの新規作成は、文字通りCreateViewを使おう
+# 21/8/28 createviewにしたことで、保存はできた→変数の受け取りなどどうするか
+# 21/8/29 form使用を復活してフィルターと保存,変数を渡すのに成功！！！！！
+class NewChoice(CreateView):
 
-class NewChoice(FormView):
-    # model = TestChoice
-    
-    form_class = TestChoiceForm
+    form_class = TestChoiceForm    # forms.pyのformを呼び出したいなら指定する
     template_name = 'new_choice.html'
-    success_url = 'stapp:new_choice'
 
-    def get(self, request, *args, **kwargs):
-        form_class = self.get_form_class()
-        form = form_class(game_id=request.GET.get('game'),start_letter=request.GET.get('start_letter'))
-
-
+    # 1.new_gameもしくは前のchoiceから変数('game','start_letter')を受け取る
+    # 2.受け取った変数を反映させたformを表示する
+    # form に kwargs を渡す
     def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs() # 継承？ようわからん
-        kwargs = {
-            'game_id': request.GET.get('game'),
-            'start_letter': request.GET.get('start_letter'), # 最初の文字。querysetをfilterする
-        }
+        kwargs = super(NewChoice, self).get_form_kwargs() # 継承。ようわからん
+        
+        # 独自で変数を追加する
+        kwargs['game'] = self.request.GET.get('game')
+        kwargs['start_letter'] = self.request.GET.get('start_letter')
+
         return kwargs
 
-        #リダイレクトはどうやる？'''
+    # 保存後に飛ぶurlを作る処理
+    def get_success_url(self):
+        
+        # 選択した駅を変数に格納
+        chosen_station = MstTestStation.objects.get(pk=self.request.POST.get('station'))
+        
+        params = urlencode({
+            'game': self.request.POST.get('game'),
+            'start_letter': chosen_station.last_letter,
+            })
+
+        # 選択画面(urlのname='new_choice')のパスを取得
+        redirect_url = reverse('stapp:new_choice')
+
+        #ゲームIDを渡して選択画面へ移動＝'views.new_choice'を呼び出す
+        url = f'{redirect_url}?{params}' # 'f'について　https://note.nkmk.me/python-f-strings/
+        
+        return url
+
+# as_view()はViewに付属の関数。
+# クロージャであるview(request, *args, **kwargs)関数を返す
+new_choice = NewChoice.as_view()
